e4ba851ed8e5be94e38717d16562c605523f333c
Maryson <U2062@in-tech.global>
2018-03-08 11:49:48 +0100

    Refactoring

--- src/App.js
+++ src/App.js
@@ -45,12 +45,12 @@ class App extends Component {
 
       cursor: 'pointer'
     };
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button style={style} onClick={this.togglePersons}>Toggle Name</button>
-        {this.state.showPersons === true ? <div>
+
+    let persons = null;
+    if(this.state.showPersons)
+    {
+      persons = (
+        <div>
           <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age} />
@@ -62,7 +62,16 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age} />
-        </div> : null}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Welcome</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersons}>Toggle Name</button>        
+        {persons}
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!! ')
