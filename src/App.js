361d0998572db61196bc7754343e7597f0e564a4
Maryson <U2062@in-tech.global>
2018-03-08 10:42:57 +0100

    Toggle (show/hide) person chages

--- src/App.js
+++ src/App.js
@@ -4,31 +4,37 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons : [
-      {name: 'Davio', age: 28},
-      {name: 'Merlin', age: 29},
-      {name: 'Lintin', age: 27}
-    ]
+    persons: [
+      { name: 'Davio', age: 28 },
+      { name: 'Merlin', age: 29 },
+      { name: 'Lintin', age: 27 }
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (arg) => {
     this.setState({
-      persons : [
-        {name: arg, age: 28},
-        {name: 'Merlin', age: 29},
-        {name: 'Lintin', age: 45}
+      persons: [
+        { name: arg, age: 28 },
+        { name: 'Jasu', age: 29 },
+        { name: 'Manu', age: 45 }
       ]
     })
   }
 
   nameChangeHandler = (event) => {
     this.setState({
-      persons : [
-        {name: 'Davio', age: 28},
-        {name: event.target.value, age: 29},
-        {name: 'Lintin', age: 45}
+      persons: [
+        { name: 'David', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Manu', age: 45 }
       ]
     })
+  }
+
+  togglePersons = () => {
+    const tmpshowPer = this.state.showPersons;
+    this.setState({ showPersons: !tmpshowPer });
   }
 
   render() {
@@ -36,25 +42,27 @@ class App extends Component {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      
+
       cursor: 'pointer'
     };
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={style} onClick={this.switchNameHandler.bind(this,'Dally')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-        <Person 
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this,'Shelto')} 
-          changed={this.nameChangeHandler}>India</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />          
+        <button style={style} onClick={this.togglePersons}>Toggle Name</button>
+        {this.state.showPersons === true ? <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Mary')}
+            changed={this.nameChangeHandler}>India</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div> : null}
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!! ')
