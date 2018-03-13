670b44bff2018d1bdedebb01e73ab90dbd4f42ce
Maryson <U2062@in-tech.global>
2018-03-13 08:06:47 +0100

    Splitting an app into components

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+    const assignedclasses = [];
+    let btnClass = '';
+    if(props.showPersons){
+        btnClass = classes.Red;
+    }    
+
+    if (props.persons.length <= 2) {
+        assignedclasses.push(classes.red); // classes = ['red']
+    }
+    if (props.persons.length <= 1) {
+        assignedclasses.push(classes.bold); // classed = ['bold']
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>Welcome</h1>
+            <p className={assignedclasses.join(' ')}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+};
+
+export default cockpit;
