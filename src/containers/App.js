1821911143bdddb83574b101e9ed28c4b6cb9a29
Maryson <U2062@in-tech.global>
2018-03-15 16:19:25 +0100

    hoc functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass'
+import Aux from '../hoc/Auxiliary'
+import withClass from '../hoc/withClass'
 
 class App extends PureComponent {
 
@@ -92,7 +93,7 @@ class App extends PureComponent {
     }
 
     return (      
-      <WithClass classes={classes.App}>
+      <Aux>
       <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -101,10 +102,10 @@ class App extends PureComponent {
           clicked={this.togglePersonsHandler}
            />
         {persons}
-      </WithClass>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
